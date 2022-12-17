eb8f65508cc446436160d8d4625a1156fbcc2d0b
Ololade0 <adesuyiololade@gmail.com>
2022-12-17 16:37:54 +0100

    Initial commit

---
+++ src/main/java/com/example/Userservice/model/Role.java
@@ -0,0 +1,23 @@
+package com.example.Userservice.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import static jakarta.persistence.GenerationType.AUTO;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = AUTO)
+    private Long id;
+    private String name;
+}
